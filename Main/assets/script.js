c97d4a5334fcd92bb9c8383404180cd81e4891ee
Jacob Sniff <jacobsniff18@gmail.com>
2023-02-19 18:24:13 -0500

    added more JS to code. Need to debug problem where on the second question, we are unable to proceed.

--- Main/assets/script.js
+++ Main/assets/script.js
@@ -10,9 +10,29 @@ var cText = document.getElementById( "answer-c" );
 var dText = document.getElementById( "answer-d" );
 var rightWrong = document.getElementById( "right-wrong" );
 var timeLeft = 60;
+var index = 0;
+var timerInterval;
+var finalButton = document.querySelector( ".final-btn" );
+var finalScore = document.getElementById( "final-score" );
+var finalForm = document.querySelector( ".final-form" );
+
+var highscorePage = document.querySelector( ".highscores-container" );
+
+var goBack = document.querySelector( ".go-back" );
+var goBack2 = document.querySelector( ".go-back-2" );
+
+var clearHighscore = document.querySelector( ".clear-highscore" );
+var viewHighscore = document.querySelector( ".highscore-btn" );
+var playAgain = document.querySelector( ".play-again-btn" );
+
+var highscore = JSON.parse( localStorage.getItem("highscores")) || [];
+var highscoreList = document.getElementById( "highscore-list" );
 
 quizPage.style.display = "none";
 lastPage.style.display = "none";
+highscorePage.style.display = "none";
+goBack.style.display = "none";
+goBack2.style.display = "none";
 
 startButton.addEventListener( 'click', function() {
     startPage.style.display = "none";
@@ -28,6 +48,10 @@ function countDown() {
 
         if (timeLeft < 0) {
             clearInterval(timerInterval);
+            quizPage.style.display = "none";
+            lastPage.style.display = "flex";
+            finalScore.innerHTML = "You did not beat the clock, until next time!"
+            finalForm.style.display = "none";
         }
     }, 1000);
 }
@@ -36,48 +60,157 @@ function deductTime(seconds) {
     timeLeft -= seconds;
 }
 
-function stopTimer() {
-    clearInterval(timerInterval);
-}
+var questions = [
+    {
+        question: "Who does Andy give the nickname 'big tuna' to?",
+        answers: [
+            { text: 'Jim Halpert', correct: true },
+            { text: 'Michael Scott', correct: false },
+            { text: 'Ryan Howard', correct: false },
+            { text: 'kevin Malone', correct: false },
+        ]
+
+    },
+    {
+        question: "Where did Jim propose to Pam?",
+        answer: [
+            { text: 'Gas Station', correct: true },
+            { text: 'Niagra Falls', correct: false },
+            { text: 'On the rooftop of Dunder Mifflin', correct: false },
+            { text: 'Inside the Office', correct: false },
+        ]
+    },
+    {
+        question: "What Ivy League school did Andy go to?",
+        answer: [
+            { text: 'Cornell', correct: true },
+            { text: 'Harvard', correct: false },
+            { text: 'Dartmouth', correct: false },
+            { text: 'Yale', correct: false },
+        ]
+    },
+    {
+        question: "How does Stanley spend his time during conference room meetings?",
+        answer: [
+            { text: 'Crossword Puzzles', correct: true },
+            { text: 'On Facebook', correct: false },
+            { text: 'Whispering with Others', correct: false },
+            { text: 'Listening to his headphones', correct: false },
+        ]
+    },
+    {
+        question: "What does Jim hide Dwight's stapler in?",
+        answer: [
+            { text: 'Jello', correct: true },
+            { text: 'Cake', correct: false },
+            { text: 'Pudding', correct: false },
+            { text: 'Glass Box', correct: false },
+        ]
+    }
+]
 
 function showQuestion(index) {
-    var questions = [
-        {
-            question: "Who does Andy give the nickname 'big tuna' to?",
-            answers: [
-                { text: 'Jim Halpert', correct: true },
-                { text: 'Michael Scott', correct: false },
-                { text: 'Ryan Howard', correct: false },
-                { text: 'kevin Malone', correct: false },
-            ]
 
-        },
-        {
-            question: "Where did Jim propose to Pam?",
-            answer: [
-                { text: 'Gas Station', correct: true },
-                { text: 'Niagra Falls', correct: false },
-                { text: 'On the rooftop of Dunder Mifflin', correct: false },
-                { text: 'Inside the Office', correct: false },
-            ]
-        },
-        {
-            question: "What Ivy League school did Andy go to?",
-            answer: [
-                { text: 'Cornell', correct: true },
-                { text: 'Harvard', correct: false },
-                { text: 'Dartmouth', correct: false },
-                { text: 'Yale', correct: false },
-            ]
-        },
-        {
-            question: "How does Stanley spend his time during conference room meetings?",
-            answer: [
-                { text: 'Crossword Puzzles', correct: true },
-                { text: 'On Facebook', correct: false },
-                { text: 'Whispering with Others', correct: false },
-                { text: 'Listening to his headphones', correct: false },
-            ]
-        }
-    ]
+    if (index >= questions.length) {
+        clearInterval(timerInterval);
+        quizPage.style.display = "none";
+        lastPage.style.display = "flex";
+        timerEl.innerHTML = "Time's up!";
+        finalScore.innerHTML = "Final Score: " + timeLeft;
+        return;
+    } 
+
+    document.getElementById( "question" ).innerHTML = questions[index].question;
+    document.getElementById( "answer-a" ).innerHTML = questions[index].answer[0].text;
+    document.getElementById( "answer-b" ).innerHTML = questions[index].answer[1].text;
+    document.getElementById( "answer-c" ).innerHTML = questions[index].answer[2].text;
+    document.getElementById( "answer-d" ).innerHTML = questions[index].answer[3].text;
 }
+
+if (index <= 3) {
+    aText.addEventListener( 'click', function() {
+        console.log(questions[index]);
+        if (questions[index].answers[0].correct === true) {
+            rightWrong.innerHTML = "Correct!";
+            index++;
+            return showQuestion(index);
+        } else {
+            rightWrong.innerHTML = "Incorrect"
+            deductTime(10);
+            index++;
+            return showQuestion(index);
+        }
+    });
+
+    bText.addEventListener( 'click', function() {
+        console.log(questions[index]);
+        if (questions[index].answers[1].correct === true) {
+            rightWrong.innerHTML = "Correct!";
+            index++;
+            return showQuestion(index);
+        } else {
+            rightWrong.innerHTML = "Incorrect"
+            deductTime(10);
+            index++;
+            return showQuestion(index);
+        }
+    });
+
+    cText.addEventListener( 'click', function() {
+        console.log(questions[index]);
+        if (questions[index].answers[2].correct === true) {
+            rightWrong.innerHTML = "Correct!";
+            index++;
+            return showQuestion(index);
+        } else {
+            rightWrong.innerHTML = "Incorrect"
+            deductTime(10);
+            index++;
+            return showQuestion(index);
+        }
+    });
+
+    dText.addEventListener( 'click', function() {
+        console.log(questions[index]);
+        if (questions[index].answers[3].correct === true) {
+            rightWrong.innerHTML = "Correct!";
+            index++;
+            return showQuestion(index);
+        } else {
+            rightWrong.innerHTML = "Incorrect"
+            deductTime(10);
+            index++;
+            return showQuestion(index);
+        }
+    });
+}
+
+finalButton.addEventListener( "click", function(event) {
+    event.preventDefault();
+    goBack.style.display = "block";
+    goBack2.style.dispaly = "none";
+
+    var initials = document.getElementById("initials").value;
+    var messageDiv = document.querySelector(".message.div");
+
+    if (initials === "" ) {
+        messageDiv.innerHTML = "Initials Here"
+        return;
+    }
+
+    highscorePage.style.display = "flex";
+    finalPage.style.display = "none";
+
+    highscore.push({ initials, timeLeft });
+
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+    console.log(highscore);
+
+    highscoreList.innerHTML = "";
+    for (var i = 0; i < highscore.length; i++) {
+        var li = document.createElement("li");
+        li.textContent = highscore[i].initials + " + " + highscore[i].timeLeft;
+        highscoreList.appendChild(li);
+    }
+
+});
